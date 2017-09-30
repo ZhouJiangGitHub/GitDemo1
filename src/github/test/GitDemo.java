dd0c71dd65269fb5fbb979fcef8a4be5244b8302
ZJGit <1505057802@qq.com>
2017-09-30 18:50:10 +0800

    Initial commit

---
+++ src/github/test/GitDemo.java
@@ -0,0 +1,10 @@
+package github.test;
+
+/**
+ * Created by zj on 2017/9/30.
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

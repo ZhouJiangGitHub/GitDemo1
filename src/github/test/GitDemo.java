5875b0bfb2dbd6e9c5d7eaef8194a078250047d7
ZJGit <1505057802@qq.com>
2017-10-01 19:40:43 +0800

    practice

--- src/github/test/GitDemo.java
+++ src/github/test/GitDemo.java
@@ -7,5 +7,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("share project on github");
+        System.out.println("111");
     }
 }

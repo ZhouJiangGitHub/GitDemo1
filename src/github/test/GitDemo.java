413dca8e8ed8ecced29515a5e7d6ecc74729ec24
ZJGit <1505057802@qq.com>
2017-10-18 17:47:58 +0800

    practice

--- src/github/test/GitDemo.java
+++ src/github/test/GitDemo.java
@@ -5,8 +5,6 @@ package github.test;
  */
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("share project on github");
         System.out.println("111");
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -12,5 +12,6 @@
   </head>
   <body>
   $END$
+  123
   </body>
 </html>
